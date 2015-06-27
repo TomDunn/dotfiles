a134a49171ae4022ed59fd6eaa10ce9b8f4429f5
tomdunn <tidunn13@gmail.com>
2015-06-27 11:10:43 -0700

    add nerdtree to vim plugins

--- vim-plugins.sh
+++ vim-plugins.sh
@@ -6,5 +6,6 @@ git clone git://github.com/tpope/vim-commentary.git
 git clone git://github.com/tpope/vim-fugitive.git
 git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
 git clone git://github.com/tpope/vim-sensible.git
+git clone https://github.com/scrooloose/nerdtree.git
 
 cd -
