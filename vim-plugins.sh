858d581a5e0a150b9f429236f1e7c71459bf222e
tomdunn <tidunn13@gmail.com>
2015-11-08 17:54:04 -0800

    install pathogen automatically

--- vim-plugins.sh
+++ vim-plugins.sh
@@ -1,4 +1,7 @@
 mkdir -p ~/.vim/bundle ~/.vim/autoload
+
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 cd ~/.vim/bundle
 
 git clone https://github.com/scrooloose/syntastic.git
